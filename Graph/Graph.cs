83d3cabe5ce90addccfe687d82481cebf3d361ee
Erik Dantas <jose.erikdantas@outlook.com>
2023-12-11 10:07:20 -0300

    finalizados metodos e testes

---
+++ Graph/Graph.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graph
+{
+    public interface IGraph<T>
+    {
+        List<List<string>> RoutesBetween(T source, T target);
+    }
+
+    public class Graph<T> : IGraph<T>
+    {
+        private IEnumerable<ILink<T>> _links;
+        private List<string> result = new List<string>();
+        private List<List<string>> routes = new List<List<string>>();
+        public Graph(IEnumerable<ILink<T>> links)
+        {
+            _links = links;
+        }
+
+        public List<List<string>> RoutesBetween(T source, T target)
+        {
+            try
+            { 
+                var sourceTarget = RecursiveRoutesBetween(source, target);
+                if(!result.Contains(source.ToString()) && result.Contains(target.ToString())){
+                    RoutesBetween(sourceTarget, target);
+                }
+                else
+                {
+                    routes.Add(result.ToList());
+                    result = new List<string>();
+                }
+                RoutesBetween(source, target);
+            }
+            catch
+            {
+                return routes;
+            }
+            return routes;
+        }
+
+        private T RecursiveRoutesBetween(T source, T target) 
+        {
+            bool foundRoute = false;
+            foreach (var link in _links)
+            {
+                bool alreadyFound = false;
+
+                if (link.Source.Equals(source))
+                {
+                    foreach (var route in routes)
+                    {
+                        if (route.Contains(link.Target.ToString()) && !link.Target.Equals(target))
+                        {
+                            alreadyFound = true;
+                            break;
+                        }
+                    }
+                    if (alreadyFound)
+                    {
+                        continue;
+                    }
+                    foundRoute = true;
+
+                    if (!result.Contains(link.Source.ToString()))
+                    {
+                        result.Add(link.Source.ToString());
+                        
+                    }
+                    if (!result.Contains(link.Target.ToString()))
+                    {
+                        result.Add(link.Target.ToString());
+                        source = link.Target;
+
+                    }
+                }
+            }
+            if (!foundRoute)
+            {
+                throw new Exception("Not found");
+            }
+            return source;
+        }
+    }
+}
--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -7,9 +7,45 @@ namespace SubtitleTimeshift
 {
     public class Shifter
     {
-        async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
+        async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding)
         {
-            throw new NotImplementedException();
+            string beginTime = string.Empty;
+            string endTime = string.Empty;
+            try
+            {
+                using (var outputWriter = new StreamWriter(output, encoding))
+                using (var file = new StreamReader(input,encoding))
+                {  
+                    string lineFile = string.Empty;
+                    while ((lineFile = file.ReadLine()) != null)
+                    {
+                        if (lineFile.Contains("-->"))
+                        {
+                            if(lineFile.Length > 12)
+                            {
+                                beginTime = lineFile.Substring(0, 12);
+                                TimeSpan beginTimeUpdated = TimeSpan.Parse(beginTime);
+                                beginTime = beginTimeUpdated.Add(timeSpan).ToString(@"hh\:mm\:ss\.fff");
+                            }
+                            if(lineFile.Length > 16)
+                            {
+                                endTime = lineFile.Substring(16);
+                                TimeSpan endTimeUpdated = TimeSpan.Parse(endTime);
+                                endTime = endTimeUpdated.Add(timeSpan).ToString(@"hh\:mm\:ss\.fff");
+                            }
+                            outputWriter.WriteLine(beginTime + " --> " + endTime);
+                        }
+                        else
+                        {
+                            outputWriter.WriteLine(lineFile);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
         }
     }
 }
